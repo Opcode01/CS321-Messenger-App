91a4ecbfd51a9c1cc272977f31ba35e01b09ed3f
Austin Vickers <adv0003@uah.edu>
2017-01-18 17:33:21 -0600

    CS 321 Messenger App v0.1
    First initial commit for the CS321 group project. Version 0.1

---
+++ src/MessengerApp/main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package MessengerApp;
+
+/**
+ * @author Austin
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
